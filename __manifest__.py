68ed43d50de81ec6ec67870143ccc3b5693f3df4
Renato <renato.vidal.a@gmail.com>
2019-03-13 11:19:18 -0400

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Comprueba el cambio diario de moneda ',
+    'version': '1.0',
+    'category': 'Accounting Management',
+    'description': """
+Use las caracteristicas contables en bolivia.
+============================================
+
+Carcteristicas:
+-----------
+- Actualice diariamiente el tipode cambio en Bolivianos, dolares, euros, UFVs
+- Abre un formulario automaticamente para que se pueda actualizar si no se ha realizado el registro del cambio del dia
+
+""",
+    'author': 'Prosebol',
+    'website': '',
+    'images': [],
+    'depends': ['account'],
+    'data': [
+        'views/account_updatechangemoney_template.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'qweb': ['static/src/xml/account_money_change_popup.xml'],
+    'active': True,
+    'installable': True,
+    'auto_install': False,
+}
