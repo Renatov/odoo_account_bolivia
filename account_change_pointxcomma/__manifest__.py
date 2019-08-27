ec7d830d4bb197a7c19ebb396f83a4f7076bfccd
Renato <renato.vidal.a@gmail.com>
2019-08-27 17:28:35 -0400

    Add files via upload

---
+++ account_change_pointxcomma/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Change point key numpad by comma',
+    'version': '1.0',
+    'category': 'Accounting Management',
+    'description': """Change point key numpad by comma.
+""",
+    'author': 'PROSBOL',
+    'website': 'https://www.prosbol.com/',
+    'images': [],
+    'license': 'AGPL-3',
+    'depends': ['account'],
+    'data': [
+        'views/res_user_config_views.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'qweb': [],
+    'active': True,
+    'installable': True,
+    'auto_install': False,
+}
